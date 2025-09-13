b644df56baf1ac441717a028175b0d950e65b345
SubhanaSaman <160390421+SubhanaSaman@users.noreply.github.com>
2025-09-13 16:37:34 +0500

    Update app.js

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ function generateQR(){
     const input = document.getElementById('text').value ;
     if(!input) return alert ("Please enter text or URl")
 
-    const QrImage = `https:api.qrserver.com/v1/create-qr-code/?size=200x200&data= ${encodeURIComponent(input)}` 
+    const QrImage = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${encodeURIComponent(input)}` 
 
 document.getElementById('QrBox').innerHTML =`<img src = "${QrImage}" alt = "QR Code">`
+
 
 }
